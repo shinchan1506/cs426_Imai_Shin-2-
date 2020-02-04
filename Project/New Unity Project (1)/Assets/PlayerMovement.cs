2da6b4b61807fb891137399ed2a1bb67cacd3218
Shin Big Computer <shin@goodprotein.net>
2020-02-04 16:25:56 -0600

    initial setup

---
+++ Project/New Unity Project (1)/Assets/PlayerMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerMovement : NetworkBehaviour
+{
+    
+    public GameObject bulletPrefab;
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isLocalPlayer)
+            return;
+        
+        var x = Input.GetAxis("Horizontal")*0.1f;
+        var z = Input.GetAxis("Vertical")*0.1f;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }
+
+        transform.Translate(x, 0, z);
+    }
+
+    void Fire()
+    {
+        // create the bullet object from the bullet prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            transform.position - transform.forward,
+            Quaternion.identity);
+
+        // make the bullet move away in front of the player
+        bullet.GetComponent<Rigidbody>().velocity = -transform.forward*4;
+        
+        // make bullet disappear after 2 seconds
+        Destroy(bullet, 2.0f);        
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# binary_birds
-426 
+# Group Name: binary birds
+
+## Group Members: Shin Imai, Angela Cuevas, Saumaun Vahedipour
+
+### Brief game idea: 
+A human is collecting birds and eggs to write binary commands to destroy an evil computer part by part.
+
+### Player Interaction Pattern: 
+Cooperative play
+
+### Objective: 
+Alignment
+
+### Procedures: 
+Who: Player (human)
+Where: Within the boundaries
+When: Before the time runs out (before the evil computer destroys the world)
+How: human moves with WASD, W: up, A: left, S: down, D: right, E to pick up or drop off bird/egg
+
+### Rules:
+Player cannot pick up more than 20 birds or eggs
+Timer of 10 minutes
+
+### Resources:
+birds and eggs
+time
+
+### Conflict:
+obstacles, timer
+
+### Boundaries:
+edges of map surrounded by forest or hill
+
+### Outcome:
+winner/loser and score(time) based
+
+### Non-plain-vanilla procedure/rule (see the Hw02 handout for what would qualify as an unusual procedure/rule):
+Player can strike a deal with the evil computer to win at the price of the other players,  but the player cannot trust the evil computer so it will be a gamble 
