6c9b0f14919299a0c0c34d05cc0ddcaf9b400724
Shin Big Computer <shin@goodprotein.net>
2020-02-07 12:53:26 -0600

    animations and camera

--- Project/New Unity Project (1)/Assets/PlayerMovement.cs
+++ Project/New Unity Project (1)/Assets/PlayerMovement.cs
@@ -5,7 +5,16 @@ using UnityEngine.Networking;
 
 public class PlayerMovement : NetworkBehaviour
 {
-    
+    Animator anim;
+    int jumpHash = Animator.StringToHash("Jump");
+    int runStateHash = Animator.StringToHash("Base Layer.Run");
+    float defaultspeed = 0.01f;
+
+    void Start ()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     public GameObject bulletPrefab;
 
     public override void OnStartLocalPlayer()
@@ -17,14 +26,33 @@ public class PlayerMovement : NetworkBehaviour
     {
         if(!isLocalPlayer)
             return;
-        
-        var x = Input.GetAxis("Horizontal")*0.1f;
-        var z = Input.GetAxis("Vertical")*0.1f;
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            Fire();
-        }
+        float move = Input.GetAxis ("Vertical");
+        float moveHoriz = Input.GetAxis("Horizontal");
+        anim.SetFloat("Speed", move);
+        anim.SetFloat("HorizSpeed", moveHoriz);
+        var x = Input.GetAxis("Horizontal")* defaultspeed;
+        var z = Input.GetAxis("Vertical")* defaultspeed;
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        // if(Input.GetKeyDown(KeyCode.Space) && stateInfo.nameHash == runStateHash)
+        // {
+        //     anim.SetTrigger (jumpHash);
+        // }
+        
+        Camera.main.transform.position = this.transform.position - this.transform.forward * 1.5f
+        + this.transform.up*1.25f;
+        Camera.main.transform.LookAt(this.transform.position);
+        Camera.main.transform.parent = this.transform;
+
+        // if (Input.GetKeyDown(KeyCode.Space))
+        // {
+        //     Fire();
+        // }
+
+        // if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.D)) {
+        //     Animation walkinganim = 
+        // }
 
         transform.Translate(x, 0, z);
     }
